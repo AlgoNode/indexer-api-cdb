algorand/indexer

Implement `GET /v2/blocks`

api/handlers.go
@@ -45,6 +45,16 @@ type ServerImplementation struct {
 // Helper functions //
 //////////////////////
 
+func validateBlockFilter(filter *idb.BlockFilter) error {
+	var errorArr = make([]string, 0)
+
+	if len(errorArr) > 0 {
+		return errors.New("invalid input: " + strings.Join(errorArr, ", "))
+	}
+
+	return nil
+}
+
 func validateTransactionFilter(filter *idb.TransactionFilter) error {
 	var errorArr = make([]string, 0)
 
@@ -981,6 +991,40 @@ func (si *ServerImplementation) LookupTransaction(ctx echo.Context, txid string)
 	return ctx.JSON(http.StatusOK, response)
 }
 
+// SearchForBlocks returns block headers matching the provided parameters
+// (GET /v2/blocks)
+func (si *ServerImplementation) SearchForBlocks(ctx echo.Context, params generated.SearchForBlocksParams) error {
+
+	// Validate query parameters
+	if err := si.verifyHandler("SearchForBlocks", ctx); err != nil {
+		return badRequest(ctx, err.Error())
+	}
+
+	// Convert query params into a filter
+	filter, err := si.blockParamsToBlockFilter(params)
+	if err != nil {
+		return badRequest(ctx, err.Error())
+	}
+	err = validateBlockFilter(&filter)
+	if err != nil {
+		return badRequest(ctx, err.Error())
+	}
+
+	// Fetch the block headers
+	blockHeaders, next, round, err := si.fetchBlockHeaders(ctx.Request().Context(), filter)
+	if err != nil {
+		return indexerError(ctx, fmt.Errorf("%s: %w", errBlockHeaderSearch, err))
+	}
+
+	// Populate the response model and render it
+	response := generated.BlocksResponse{
+		CurrentRound: round,
+		NextToken:    strPtr(next),
+		Blocks:       blockHeaders,
+	}
+	return ctx.JSON(http.StatusOK, response)
+}
+
 // SearchForTransactions returns transactions matching the provided parameters
 // (GET /v2/transactions)
 func (si *ServerImplementation) SearchForTransactions(ctx echo.Context, params generated.SearchForTransactionsParams) error {
@@ -1414,6 +1458,46 @@ func (si *ServerImplementation) fetchAccounts(ctx context.Context, options idb.A
 		return nil, 0, err
 	}
 	return accounts, round, nil
+}
+
+// fetchBlockHeaders is used to query the backend for block headers, and compute the next token
+func (si *ServerImplementation) fetchBlockHeaders(ctx context.Context, bf idb.BlockFilter) ([]generated.Block, string, uint64 /*round*/, error) {
+
+	var round uint64
+	var nextToken string
+	results := make([]generated.Block, 0)
+	err := callWithTimeout(ctx, si.log, si.timeout, func(ctx context.Context) error {
+
+		// Open a channel from which result rows will be received
+		var rows <-chan idb.BlockRow
+		rows, round = si.db.Blocks(ctx, bf)
+
+		// Iterate recieved rows, converting each to a generated.Block
+		var lastRow idb.BlockRow
+		for row := range rows {
+			if row.Error != nil {
+				return row.Error
+			}
+
+			results = append(results, rowToBlock(&row.BlockHeader))
+			lastRow = row
+		}
+
+		// No next token if there were no results.
+		if len(results) == 0 {
+			return nil
+		}
+
+		// Generate the next token and return
+		var err error
+		nextToken, err = lastRow.Next()
+		return err
+	})
+	if err != nil {
+		return nil, "", 0, err
+	}
+
+	return results, nextToken, round, nil
 }
 
 // fetchTransactions is used to query the backend for transactions, and compute the next token

idb/postgres/postgres.go
@@ -723,6 +723,53 @@ func buildTransactionQuery(tf idb.TransactionFilter) (query string, whereArgs []
 	return
 }
 
+func buildBlockQuery(bf idb.BlockFilter) (query string, whereArgs []interface{}, err error) {
+
+	whereParts := make([]string, 0)
+	whereArgs = make([]interface{}, 0)
+	partNumber := 1
+
+	if bf.MinRound != 0 {
+		whereParts = append(whereParts, fmt.Sprintf("round >= $%d", partNumber))
+		whereArgs = append(whereArgs, bf.MinRound)
+		partNumber++
+	}
+
+	// SELECT, FROM
+	query = `SELECT header FROM block_header`
+	// WHERE
+	if len(whereParts) > 0 {
+		whereStr := strings.Join(whereParts, " AND ")
+		query += " WHERE " + whereStr
+	}
+	// ORDER BY
+	query += " ORDER BY round ASC"
+	// LIMIT
+	if bf.Limit != 0 {
+		query += fmt.Sprintf(" LIMIT %d", bf.Limit)
+	}
+	return
+}
+
+// This function blocks. `tx` must be non-nil.
+func (db *IndexerDb) yieldBlocks(ctx context.Context, tx pgx.Tx, bf idb.BlockFilter, out chan<- idb.BlockRow) {
+
+	query, whereArgs, err := buildBlockQuery(bf)
+	if err != nil {
+		err = fmt.Errorf("block query err %v", err)
+		out <- idb.BlockRow{Error: err}
+		return
+	}
+
+	rows, err := tx.Query(ctx, query, whereArgs...)
+	if err != nil {
+		err = fmt.Errorf("block query %#v err %v", query, err)
+		out <- idb.BlockRow{Error: err}
+		return
+	}
+	db.yieldBlocksThreadSimple(rows, out)
+}
+
 // This function blocks. `tx` must be non-nil.
 func (db *IndexerDb) yieldTxns(ctx context.Context, tx pgx.Tx, tf idb.TransactionFilter, out chan<- idb.TxnRow) {
 	if len(tf.NextToken) > 0 {
@@ -767,6 +814,42 @@ func txnFilterOptimization(tf idb.TransactionFilter) idb.TransactionFilter {
 		tf.SkipInnerTransactions = true
 	}
 	return tf
+}
+
+// Blocks is part of idb.IndexerDB
+func (db *IndexerDb) Blocks(ctx context.Context, bf idb.BlockFilter) (<-chan idb.BlockRow, uint64) {
+	out := make(chan idb.BlockRow, 1)
+
+	tx, err := db.db.BeginTx(ctx, readonlyRepeatableRead)
+	if err != nil {
+		out <- idb.BlockRow{Error: err}
+		close(out)
+		return out, 0
+	}
+
+	round, err := db.getMaxRoundAccounted(ctx, tx)
+	if err != nil {
+		out <- idb.BlockRow{Error: err}
+		close(out)
+		if rerr := tx.Rollback(ctx); rerr != nil {
+			db.log.Printf("rollback error: %s", rerr)
+		}
+		return out, round
+	}
+
+	go func() {
+		db.yieldBlocks(ctx, tx, bf, out)
+		// Because we return a channel into a "callWithTimeout" function,
+		// We need to make sure that rollback is called before close()
+		// otherwise we can end up with a situation where "callWithTimeout"
+		// will cancel our context, resulting in connection pool churn
+		if rerr := tx.Rollback(ctx); rerr != nil {
+			db.log.Printf("rollback error: %s", rerr)
+		}
+		close(out)
+	}()
+
+	return out, round
 }
 
 // Transactions is part of idb.IndexerDB
@@ -891,6 +974,30 @@ func (db *IndexerDb) txnsWithNext(ctx context.Context, tx pgx.Tx, tf idb.Transac
 		return
 	}
 	db.yieldTxnsThreadSimple(rows, out, nil, nil)
+}
+
+func (db *IndexerDb) yieldBlocksThreadSimple(rows pgx.Rows, results chan<- idb.BlockRow) {
+	defer rows.Close()
+
+	for rows.Next() {
+		var row idb.BlockRow
+
+		var blockheaderjson []byte
+		err := rows.Scan(&blockheaderjson)
+		if err != nil {
+			row.Error = err
+		} else {
+			row.BlockHeader, err = encoding.DecodeBlockHeader(blockheaderjson)
+			if err != nil {
+				row.Error = fmt.Errorf("failed to decode block header: %w", err)
+			}
+		}
+
+		results <- row
+	}
+	if err := rows.Err(); err != nil {
+		results <- idb.BlockRow{Error: err}
+	}
 }
 
 func (db *IndexerDb) yieldTxnsThreadSimple(rows pgx.Rows, results chan<- idb.TxnRow, countp *int, errp *error) {

AlgoNode/indexer-api-cdb

api/handlers.go
@@ -1008,15 +1008,16 @@ func (si *ServerImplementation) LookupTransaction(ctx echo.Context, txid string)
 }
 
 // SearchForBlockHeaders returns block headers matching the provided parameters
-// (GET /v2/blocks)
+// (GET /v2/block-headers)
 func (si *ServerImplementation) SearchForBlockHeaders(ctx echo.Context, params generated.SearchForBlockHeadersParams) error {
+
 	// Validate query parameters
 	if err := si.verifyHandler("SearchForBlockHeaders", ctx); err != nil {
 		return badRequest(ctx, err.Error())
 	}
 
 	// Convert query params into a filter
-	filter, err := si.blockParamsToBlockFilter(params)
+	filter, err := si.blockHeadersParamsToBlockHeadersFilter(params)
 	if err != nil {
 		return badRequest(ctx, err.Error())
 	}
@@ -1035,49 +1036,43 @@ func (si *ServerImplementation) SearchForBlockHeaders(ctx echo.Context, params g
 	response := generated.BlockHeadersResponse{
 		CurrentRound: round,
 		NextToken:    strPtr(next),
-		Blocks:       blockHeaders,
+		BlockHeaders: blockHeaders,
 	}
 	return ctx.JSON(http.StatusOK, response)
 }
 
-// fetchBlockHeaders is used to query the backend for block headers, and compute the next token
-func (si *ServerImplementation) fetchBlockHeaders(ctx context.Context, bf idb.BlockHeaderFilter) ([]generated.Block, string, uint64 /*round*/, error) {
+// SearchForBlocks returns block headers matching the provided parameters
+// (GET /v2/blocks)
+func (si *ServerImplementation) SearchForBlocks(ctx echo.Context, params generated.SearchForBlocksParams) error {
 
-	var round uint64
-	var nextToken string
-	results := make([]generated.Block, 0)
-	err := callWithTimeout(ctx, si.log, si.timeout, func(ctx context.Context) error {
-
-		// Open a channel from which result rows will be received
-		var rows <-chan idb.BlockRow
-		rows, round = si.db.BlockHeaders(ctx, bf)
-
-		// Iterate received rows, converting each to a generated.Block
-		var lastRow idb.BlockRow
-		for row := range rows {
-			if row.Error != nil {
-				return row.Error
-			}
-
-			results = append(results, hdrRowToBlock(row))
-			lastRow = row
-		}
-
-		// No next token if there were no results.
-		if len(results) == 0 {
-			return nil
-		}
-
-		// Generate the next token and return
-		var err error
-		nextToken, err = lastRow.Next()
-		return err
-	})
-	if err != nil {
-		return nil, "", 0, err
+	// Validate query parameters
+	if err := si.verifyHandler("SearchForBlocks", ctx); err != nil {
+		return badRequest(ctx, err.Error())
 	}
 
-	return results, nextToken, round, nil
+	// Convert query params into a filter
+	filter, err := si.blockParamsToBlockHeadersFilter(params)
+	if err != nil {
+		return badRequest(ctx, err.Error())
+	}
+	err = validateBlockFilter(&filter)
+	if err != nil {
+		return badRequest(ctx, err.Error())
+	}
+
+	// Fetch the block headers
+	blockHeaders, next, round, err := si.fetchBlockHeaders(ctx.Request().Context(), filter)
+	if err != nil {
+		return indexerError(ctx, fmt.Errorf("%s: %w", errBlockHeaderSearch, err))
+	}
+
+	// Populate the response model and render it
+	response := generated.BlockHeadersResponse{
+		CurrentRound: round,
+		NextToken:    strPtr(next),
+		BlockHeaders: blockHeaders,
+	}
+	return ctx.JSON(http.StatusOK, response)
 }
 
 // SearchForTransactions returns transactions matching the provided parameters
@@ -1513,6 +1508,46 @@ func (si *ServerImplementation) fetchAccounts(ctx context.Context, options idb.A
 		return nil, 0, err
 	}
 	return accounts, round, nil
+}
+
+// fetchBlockHeaders is used to query the backend for block headers, and compute the next token
+func (si *ServerImplementation) fetchBlockHeaders(ctx context.Context, bf idb.BlockHeaderFilter) ([]generated.BlockHeader, string, uint64 /*round*/, error) {
+
+	var round uint64
+	var nextToken string
+	results := make([]generated.BlockHeader, 0)
+	err := callWithTimeout(ctx, si.log, si.timeout, func(ctx context.Context) error {
+
+		// Open a channel from which result rows will be received
+		var rows <-chan idb.BlockHeaderRow
+		rows, round = si.db.BlockHeaders(ctx, bf)
+
+		// Iterate recieved rows, converting each to a generated.Block
+		var lastRow idb.BlockHeaderRow
+		for row := range rows {
+			if row.Error != nil {
+				return row.Error
+			}
+
+			results = append(results, hdrRowToBlock(row))
+			lastRow = row
+		}
+
+		// No next token if there were no results.
+		if len(results) == 0 {
+			return nil
+		}
+
+		// Generate the next token and return
+		var err error
+		nextToken, err = lastRow.Next()
+		return err
+	})
+	if err != nil {
+		return nil, "", 0, err
+	}
+
+	return results, nextToken, round, nil
 }
 
 // fetchTransactions is used to query the backend for transactions, and compute the next token

idb/postgres/postgres.go
@@ -723,6 +723,53 @@ func buildTransactionQuery(tf idb.TransactionFilter) (query string, whereArgs []
 	return
 }
 
+func buildBlockQuery(bf idb.BlockHeaderFilter) (query string, whereArgs []interface{}, err error) {
+
+	whereParts := make([]string, 0)
+	whereArgs = make([]interface{}, 0)
+	partNumber := 1
+
+	if bf.MinRound != nil {
+		whereParts = append(whereParts, fmt.Sprintf("round >= $%d", partNumber))
+		whereArgs = append(whereArgs, bf.MinRound)
+		partNumber++
+	}
+
+	// SELECT, FROM
+	query = `SELECT header FROM block_header`
+	// WHERE
+	if len(whereParts) > 0 {
+		whereStr := strings.Join(whereParts, " AND ")
+		query += " WHERE " + whereStr
+	}
+	// ORDER BY
+	query += " ORDER BY round ASC"
+	// LIMIT
+	if bf.Limit != 0 {
+		query += fmt.Sprintf(" LIMIT %d", bf.Limit)
+	}
+	return
+}
+
+// This function blocks. `tx` must be non-nil.
+func (db *IndexerDb) yieldBlocks(ctx context.Context, tx pgx.Tx, bf idb.BlockHeaderFilter, out chan<- idb.BlockHeaderRow) {
+
+	query, whereArgs, err := buildBlockQuery(bf)
+	if err != nil {
+		err = fmt.Errorf("block query err %v", err)
+		out <- idb.BlockHeaderRow{Error: err}
+		return
+	}
+
+	rows, err := tx.Query(ctx, query, whereArgs...)
+	if err != nil {
+		err = fmt.Errorf("block query %#v err %v", query, err)
+		out <- idb.BlockHeaderRow{Error: err}
+		return
+	}
+	db.yieldBlocksThreadSimple(rows, out)
+}
+
 // This function blocks. `tx` must be non-nil.
 func (db *IndexerDb) yieldTxns(ctx context.Context, tx pgx.Tx, tf idb.TransactionFilter, out chan<- idb.TxnRow) {
 	if len(tf.NextToken) > 0 {
@@ -767,6 +814,42 @@ func txnFilterOptimization(tf idb.TransactionFilter) idb.TransactionFilter {
 		tf.SkipInnerTransactions = true
 	}
 	return tf
+}
+
+// Blocks is part of idb.IndexerDB
+func (db *IndexerDb) Blocks(ctx context.Context, bf idb.BlockHeaderFilter) (<-chan idb.BlockHeaderRow, uint64) {
+	out := make(chan idb.BlockHeaderRow, 1)
+
+	tx, err := db.db.BeginTx(ctx, readonlyRepeatableRead)
+	if err != nil {
+		out <- idb.BlockHeaderRow{Error: err}
+		close(out)
+		return out, 0
+	}
+
+	round, err := db.getMaxRoundAccounted(ctx, tx)
+	if err != nil {
+		out <- idb.BlockHeaderRow{Error: err}
+		close(out)
+		if rerr := tx.Rollback(ctx); rerr != nil {
+			db.log.Printf("rollback error: %s", rerr)
+		}
+		return out, round
+	}
+
+	go func() {
+		db.yieldBlocks(ctx, tx, bf, out)
+		// Because we return a channel into a "callWithTimeout" function,
+		// We need to make sure that rollback is called before close()
+		// otherwise we can end up with a situation where "callWithTimeout"
+		// will cancel our context, resulting in connection pool churn
+		if rerr := tx.Rollback(ctx); rerr != nil {
+			db.log.Printf("rollback error: %s", rerr)
+		}
+		close(out)
+	}()
+
+	return out, round
 }
 
 // Transactions is part of idb.IndexerDB
@@ -891,6 +974,30 @@ func (db *IndexerDb) txnsWithNext(ctx context.Context, tx pgx.Tx, tf idb.Transac
 		return
 	}
 	db.yieldTxnsThreadSimple(rows, out, nil, nil)
+}
+
+func (db *IndexerDb) yieldBlocksThreadSimple(rows pgx.Rows, results chan<- idb.BlockHeaderRow) {
+	defer rows.Close()
+
+	for rows.Next() {
+		var row idb.BlockHeaderRow
+
+		var blockheaderjson []byte
+		err := rows.Scan(&blockheaderjson)
+		if err != nil {
+			row.Error = err
+		} else {
+			row.BlockHeader, err = encoding.DecodeBlockHeader(blockheaderjson)
+			if err != nil {
+				row.Error = fmt.Errorf("failed to decode block header: %w", err)
+			}
+		}
+
+		results <- row
+	}
+	if err := rows.Err(); err != nil {
+		results <- idb.BlockHeaderRow{Error: err}
+	}
 }
 
 func (db *IndexerDb) yieldTxnsThreadSimple(rows pgx.Rows, results chan<- idb.TxnRow, countp *int, errp *error) {
@@ -1062,38 +1169,38 @@ func buildBlockHeadersQuery(bf idb.BlockHeaderFilter) (query string, err error) 
 }
 
 // This function blocks. `tx` must be non-nil.
-func (db *IndexerDb) yieldBlockHeaders(ctx context.Context, tx pgx.Tx, bf idb.BlockHeaderFilter, out chan<- idb.BlockRow) {
+func (db *IndexerDb) yieldBlockHeaders(ctx context.Context, tx pgx.Tx, bf idb.BlockHeaderFilter, out chan<- idb.BlockHeaderRow) {
 
 	query, err := buildBlockHeadersQuery(bf)
 	if err != nil {
 		err = fmt.Errorf("block query err %v", err)
-		out <- idb.BlockRow{Error: err}
+		out <- idb.BlockHeaderRow{Error: err}
 		return
 	}
 
 	rows, err := tx.Query(ctx, query)
 	if err != nil {
 		err = fmt.Errorf("block query %#v err %v", query, err)
-		out <- idb.BlockRow{Error: err}
+		out <- idb.BlockHeaderRow{Error: err}
 		return
 	}
 	db.yieldBlocksThreadSimple(rows, out)
 }
 
 // BlockHeaders is part of idb.IndexerDB
-func (db *IndexerDb) BlockHeaders(ctx context.Context, bf idb.BlockHeaderFilter) (<-chan idb.BlockRow, uint64) {
-	out := make(chan idb.BlockRow, 1)
+func (db *IndexerDb) BlockHeaders(ctx context.Context, bf idb.BlockHeaderFilter) (<-chan idb.BlockHeaderRow, uint64) {
+	out := make(chan idb.BlockHeaderRow, 1)
 
 	tx, err := db.db.BeginTx(ctx, readonlyRepeatableRead)
 	if err != nil {
-		out <- idb.BlockRow{Error: err}
+		out <- idb.BlockHeaderRow{Error: err}
 		close(out)
 		return out, 0
 	}
 
 	round, err := db.getMaxRoundAccounted(ctx, tx)
 	if err != nil {
-		out <- idb.BlockRow{Error: err}
+		out <- idb.BlockHeaderRow{Error: err}
 		close(out)
 		if rerr := tx.Rollback(ctx); rerr != nil {
 			db.log.Printf("rollback error: %s", rerr)
@@ -1114,30 +1221,6 @@ func (db *IndexerDb) BlockHeaders(ctx context.Context, bf idb.BlockHeaderFilter)
 	}()
 
 	return out, round
-}
-
-func (db *IndexerDb) yieldBlocksThreadSimple(rows pgx.Rows, results chan<- idb.BlockRow) {
-	defer rows.Close()
-
-	for rows.Next() {
-		var row idb.BlockRow
-
-		var blockheaderjson []byte
-		err := rows.Scan(&blockheaderjson)
-		if err != nil {
-			row.Error = err
-		} else {
-			row.BlockHeader, err = encoding.DecodeBlockHeader(blockheaderjson)
-			if err != nil {
-				row.Error = fmt.Errorf("failed to decode block header: %w", err)
-			}
-		}
-
-		results <- row
-	}
-	if err := rows.Err(); err != nil {
-		results <- idb.BlockRow{Error: err}
-	}
 }
 
 var statusStrings = []string{"Offline", "Online", "NotParticipating"}